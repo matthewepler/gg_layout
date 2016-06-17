c062ae3c5fcbdb807d89a5446959ec369eb36f13
Matthew Epler <matthewepler@gmail.com>
2016-06-17 18:58:51 -0400

    added prev work files

---
+++ gg_htmlTest/main.js
@@ -0,0 +1,11 @@
+// window.onload = function() {
+// 	var rem = 16;
+// 	var quote = document.getElementsByClassName('ref-quote')[0];
+// 	var box = quote.getBoundingClientRect();
+// 	console.log(box.left);
+// 	var details = document.getElementsByClassName('details-wrapper');
+// 	var i;
+// 	for (i=0; i<details.length; i++) {
+// 		details[i].style.paddingLeft = box.left + (rem*1.5) + "px";
+// 	}
+// }
